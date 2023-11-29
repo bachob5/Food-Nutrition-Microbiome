92a14702f26ce6063560deb1aeed166f508015c2
BachirB <balechbachir@gmail.com>
2023-11-29 13:11:41 +0100

    Get related results
    Automatic retrieval of studies results

---
+++ getRelatedStudyResults.py
@@ -0,0 +1,77 @@
+import sys
+import requests as rq
+import pandas as pd
+
+InFile=sys.argv[1]
+
+def get_StudyId_Links(InFile):
+    D_studyID_linkResults={}
+    D_studyID_linkSamples={}
+    D_studyID_linkPubblications={}
+    D_studyID_linkAnalyses={}
+    df=pd.read_csv(InFile)
+    for ix, nm in enumerate(df['id']):
+        D_studyID_linkResults[nm]=df['relationships.downloads.links.related'][ix]
+        D_studyID_linkSamples[nm]=df['relationships.samples.links.related'][ix]
+        D_studyID_linkPubblications[nm]=df['relationships.publications.links.related'][ix]
+        D_studyID_linkAnalyses[nm]=df['relationships.analyses.links.related'][ix]
+    return D_studyID_linkResults, D_studyID_linkSamples, D_studyID_linkPubblications, D_studyID_linkAnalyses
+
+StudyID_links=get_StudyId_Links(InFile)
+Ress=StudyID_links[0]
+Samp=StudyID_links[1]
+Pubb=StudyID_links[2]
+Analyses=StudyID_links[3]
+
+
+def GET_Analysis(Link2Analysis):
+    res=rq.get(Link2Analysis)
+    return res
+
+def retrieveData(LINK):
+    res=rq.get(LINK)
+    res1=res.json()['data']
+    return res1
+
+def transformJson2csv(res_json):
+    df=pd.json_normalize(res_json)
+    return df
+
+
+#Get analysis data information
+
+for sID in Ress:
+    Link2Results=Ress[sID]
+    #print (sID, Link2Results)
+    r=GET_Analysis(Link2Results)
+    #Get results in json format
+    d=r.json()
+    AllData=d['links']
+    #print (AllData)
+    baseLink=AllData['first']
+    ParsedLink=baseLink.split('&')
+    #print (ParsedLink)
+    NumPages=d['meta']['pagination']['pages']
+    #print(NumPages)      
+    AllLinks=[]
+    if NumPages==1:
+        AllLinks.append(ParsedLink[0])
+    elif NumPages > 1:
+        for n in range(NumPages):
+            tmp=ParsedLink[1].split('=')
+            tmp.pop()
+            tmp.append(str(n+1))
+            currPage='='.join(tmp)
+            ParsedLink.remove(ParsedLink[1])
+            ParsedLink.insert(1, currPage)
+            newLink='&'.join(ParsedLink)
+            AllLinks.append(newLink)
+    #print (sID, AllLinks)
+    f_out=sID+'_RelatedResults.csv'
+    res_dfs=[]
+    for ln in AllLinks:
+        res_ln = retrieveData(ln)
+        res_df=transformJson2csv(res_ln)
+        res_dfs.append(res_df)
+        res_all=pd.concat(res_dfs, ignore_index=True)
+        res_all.to_csv(f_out)
