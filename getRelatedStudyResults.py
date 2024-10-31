e6c0ed028aed9ce797558ff86926d10c26019a62
BachirB <balechbachir@gmail.com>
2024-10-31 15:47:41 +0100

    Update getRelatedStudyResults.py
    Fixed error return due to empty links to the results

--- getRelatedStudyResults.py
+++ getRelatedStudyResults.py
@@ -2,6 +2,8 @@ import sys
 import requests as rq
 import pandas as pd
 
+''' This scrpt should be fixed to account for empty columns resulting in API error while retrieving the results from Mgnify'''
+
 InFile=sys.argv[1]
 
 def get_StudyId_Links(InFile):
@@ -42,36 +44,37 @@ def transformJson2csv(res_json):
 
 for sID in Ress:
     Link2Results=Ress[sID]
-    #print (sID, Link2Results)
-    r=GET_Analysis(Link2Results)
-    #Get results in json format
-    d=r.json()
-    AllData=d['links']
-    #print (AllData)
-    baseLink=AllData['first']
-    ParsedLink=baseLink.split('&')
-    #print (ParsedLink)
-    NumPages=d['meta']['pagination']['pages']
-    #print(NumPages)      
-    AllLinks=[]
-    if NumPages==1:
-        AllLinks.append(ParsedLink[0])
-    elif NumPages > 1:
-        for n in range(NumPages):
-            tmp=ParsedLink[1].split('=')
-            tmp.pop()
-            tmp.append(str(n+1))
-            currPage='='.join(tmp)
-            ParsedLink.remove(ParsedLink[1])
-            ParsedLink.insert(1, currPage)
-            newLink='&'.join(ParsedLink)
-            AllLinks.append(newLink)
-    #print (sID, AllLinks)
-    f_out=sID+'_RelatedResults.csv'
-    res_dfs=[]
-    for ln in AllLinks:
-        res_ln = retrieveData(ln)
-        res_df=transformJson2csv(res_ln)
-        res_dfs.append(res_df)
-        res_all=pd.concat(res_dfs, ignore_index=True)
-        res_all.to_csv(f_out)
+    #print (sID, Link2Results, type(Link2Results))
+    if str(Link2Results).startswith('https:'): #This checks whether it is a web link
+        r=GET_Analysis(Link2Results)
+        #Get results in json format
+        d=r.json()
+        AllData=d['links']
+        #print (AllData)
+        baseLink=AllData['first']
+        ParsedLink=baseLink.split('&')
+        #print (ParsedLink)
+        NumPages=d['meta']['pagination']['pages']
+        #print(NumPages)      
+        AllLinks=[]
+        if NumPages==1:
+            AllLinks.append(ParsedLink[0])
+        elif NumPages > 1:
+            for n in range(NumPages):
+                tmp=ParsedLink[1].split('=')
+                tmp.pop()
+                tmp.append(str(n+1))
+                currPage='='.join(tmp)
+                ParsedLink.remove(ParsedLink[1])
+                ParsedLink.insert(1, currPage)
+                newLink='&'.join(ParsedLink)
+                AllLinks.append(newLink)
+        #print (sID, AllLinks)
+        f_out=sID+'_RelatedResults.csv'
+        res_dfs=[]
+        for ln in AllLinks:
+            res_ln = retrieveData(ln)
+            res_df=transformJson2csv(res_ln)
+            res_dfs.append(res_df)
+            res_all=pd.concat(res_dfs, ignore_index=True)
+            res_all.to_csv(f_out)
